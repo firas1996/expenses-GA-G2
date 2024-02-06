ce570982dbf0560d89fa539aba3595c7dfec820e
firas hamila <firas.hm.1996@gmail.com>
2024-02-06 09:51:04 +0100

    hh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expensesList = [
+    {
+      id: 1,
+      title: "Car",
+      price: 5000,
+      date: new Date(2024, 5, 20),
+    },
+    {
+      id: 2,
+      title: "Education",
+      price: 7200,
+      date: new Date(2023, 10, 15),
+    },
+    {
+      id: 3,
+      title: "New PC",
+      price: 3200,
+      date: new Date(2023, 3, 1),
+    },
+    {
+      id: 4,
+      title: "Voyage",
+      price: 2500,
+      date: new Date(2025, 7, 30),
+    },
+  ];
   return (
     <div>
-      <ExpenseItem />
+      {expensesList.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            price={expense.price}
+            date={expense.date}
+            // key={expense.id}
+          />
+        );
+      })}
     </div>
   );
 }
